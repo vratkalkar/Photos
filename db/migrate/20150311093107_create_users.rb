10c8cbb2ee64185674f89238c7f65a0570fa67ba
Vivek Ratkalkar <vratkalkar@live.com>
2015-03-11 10:02:00 -0400

    Add signup and login button to index

--- db/migrate/20150311093107_create_users.rb
+++ db/migrate/20150311093107_create_users.rb
@@ -1,11 +1,5 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-      t.string :name, null: false
-      t.string :email, null: false
-      t.string :password_digest
-
-      t.timestamps
-    end
+    create_table :users do
   end
 end
