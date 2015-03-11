fc17d3dc67e2ee019057ec2d0a27db4cd31d5d32
Vivek Ratkalkar <vratkalkar@live.com>
2015-03-11 10:07:11 -0400

    fix photos table

--- db/migrate/20150311093131_create_photos.rb
+++ db/migrate/20150311093131_create_photos.rb
@@ -1,6 +1,6 @@
 class CreatePhotos < ActiveRecord::Migration
   def change
-    create_table do |t|
+    create_table :photos do |t|
       t.string :name, null: false
       t.string :photo_url, null: false
       t.references :user
