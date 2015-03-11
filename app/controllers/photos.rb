7f5cc2cbf36506005a92205a336abd0c803ef214
Vivek Ratkalkar <vratkalkar@live.com>
2015-03-11 12:02:21 -0400

    Photo uploads to database

--- app/controllers/photos.rb
+++ app/controllers/photos.rb
@@ -1,9 +1,24 @@
 get '/photos/new' do
 end
 
-get '/photos/uploader' do
+get '/categories/:category_id/photos/uploader' do |category_id|
+  @category = Category.find(category_id)
   erb :"photo/uploader"
 end
 
-post '/photos/upload' do
+post '/photos/uploader' do
+  p params
+  @filename = params[:file][:filename]
+  file = params[:file][:tempfile]
+  photo_url = "public/img/#{@filename}"
+
+  File.open("public/img/#{@filename}", "wb") do |f|
+    f.write(file.read)
+  end
+
+  category = Category.find(params[:category_id])
+  category.photos.create(params[:photo].merge({photo_url: photo_url}))
+
+  redirect "/categories/#{category.id}"
+
 end
