d4d08a51015493115b4e217aaaacbd1c6d7b8c76
Vivek Ratkalkar <vratkalkar@live.com>
2015-03-11 19:48:07 -0400

    Add Photo model spec

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -4,11 +4,11 @@ describe Photo do
 let(:photo) {Photo.create(name: "Demo Photo", photo_url: "www.photo.com/new_photo")}
 
   it "has a populated name field" do
-    expect(photo.name).to be_valid
+    expect(photo.name).to eq("Demo Photo")
   end
 
   it "has a populated photo url" do
-    expect(photo.photo_url).to be_valid
+    expect(photo.photo_url).to eq("www.photo.com/new_photo")
   end
 
 end
