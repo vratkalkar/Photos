4425c75c5aa9ce7bd75741daf4bdf0ec4fedbb3d
Vivek Ratkalkar <vratkalkar@live.com>
2015-03-11 15:01:40 -0400

    Added ajax get to display login form

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,19 @@
 // Javascript Goes Here~
 //
+
+$(document).ready(function(){
+  $('#logIn').on('click', function(event){
+    event.preventDefault();
+
+    $.ajax({
+    url:'/login',
+    type: 'GET',
+    success: function(response){
+      $("#index_page").html(response);
+    },
+    error: function(response){
+      alert("This is an error");
+    },
+   });
+  });
+});
