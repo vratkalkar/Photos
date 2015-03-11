ec0b90fc7780281e2409ca5d8f5dadada56b6d03
Vivek Ratkalkar <vratkalkar@live.com>
2015-03-11 09:50:38 -0400

    Created tables

--- db/migrate/20150311093131_create_photos.rb
+++ db/migrate/20150311093131_create_photos.rb
@@ -1,4 +1,12 @@
 class CreatePhotos < ActiveRecord::Migration
   def change
+    create_table do |t|
+      t.string :name, null: false
+      t.string :photo_url, null: false
+      t.references :user
+      t.references :category
+
+      t.timestamps
+    end
   end
 end
