ce13a1801c628e6cde582f9f0cc5fbf718e4c27c
Vivek Ratkalkar <vratkalkar@live.com>
2015-10-01 14:43:40 -0400

    code review to update

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -16,15 +16,6 @@ get '/login' do
   erb :"auth/login"
 end
 
-# post '/login' do
-#   @user = User.find_by(email: params[:user][:email])
-#    if @user.try(:authenticate, params[:user][:password])
-#     session[:user_id] = @user.id
-#     redirect '/categories'
-#    else
-#     redirect '/login'
-#   end
-# end
 
 post '/login.json' do
   @user = User.find_by(email: params[:user][:email])
